a12f4467202c185b64e6db51b2f883b73e130b42
Lincoln Pires <lincolnpires@users.noreply.github.com>
2021-04-23 13:07:48 +0200

    Make simple site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './buggycloud.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          buggycloud
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn a bit
         </a>
       </header>
     </div>
